54d5edc76a0775c2d89b369ddd5354c26d7f4721
Tejaswini-A <tejaswiniamrnath@gmail.com>
2024-10-02 16:15:26 +0530

    modules

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,15 @@ output "vpc_id" {
 output "ig_id" {
   value       = module.vpc.ig_id
 }
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,12 @@
 module "vpc" {
-    source = "../terraform-aws-vpc"
+   # source = "../terraform-aws-vpc"
+    source = "git::https://github.com/Tejaswini-A-daws81s/terraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     environment = var.environment
     common_tags = var.common_tags
     public_cidrs = var.public_cidrs
     private_cidrs = var.private_cidrs
     database_cidrs = var.database_cidrs
-    is_peering_required = true
+    is_peering_required = var.is_peering_required
 
 }
